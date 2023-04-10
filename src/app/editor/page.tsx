02fe8ec510eea36f1bf77f819225dca2edd3f967
unknown <hjy4649@naver.com>
2023-04-10 11:09:04 +0900

    이상태가 데이터저장됨

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -17,26 +17,38 @@ export default function Editor() {
   const editorRef = useRef(null);
   const Editor = new EditorJS({
     holder: "editorjs",
-    autofocus: true,
-    tools: {
-      header: {
-        class: Header,
-        inlineToolbar: ["link"],
-      },
-      list: {
-        class: List,
-        inlineToolbar: true,
-      },
+    // autofocus: true,
+    // tools: {
+    //   header: {
+    //     class: Header,
+    //     inlineToolbar: ["link"],
+    //   },
+    //   list: {
+    //     class: List,
+    //     inlineToolbar: true,
+    //   },
 
-      embed: Embed,
-      link: Link,
-      raw: Raw,
-      SimpleImage: SimpleImage,
-      checklist: CheckList,
-      image: editorImage,
-      quote: Quote,
-    },
-    // data: {},
+    //   embed: Embed,
+    //   link: Link,
+    //   raw: Raw,
+    //   SimpleImage: SimpleImage,
+    //   checklist: CheckList,
+    //   image: editorImage,
+    //   quote: Quote,
+    // },
+
+    // data: {
+    //   time: 1552744582955,
+    //   blocks: [
+    //     {
+    //       type: "image",
+    //       data: {
+    //         url: "https://cdn.pixabay.com/photo/2017/09/01/21/53/blue-2705642_1280.jpg",
+    //       },
+    //     },
+    //   ],
+    //   version: "2.11.10",
+    // },
   });
   const saveData = async () => {
     const data = await Editor.save();
@@ -50,9 +62,14 @@ export default function Editor() {
     };
   }, []);
   return (
-    <div>
-      <button onClick={saveData}>저장</button>
-      <div id="editorjs" ref={editorRef} />
+    <div className="Editor">
+      <h1>editor js입니다.</h1>
+      <div>
+        <button onClick={saveData} className="Editor-btn">
+          저장
+        </button>
+        <div id="editorjs" ref={editorRef} />
+      </div>
     </div>
   );
 }
