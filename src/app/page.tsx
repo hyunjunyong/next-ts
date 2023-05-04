49a924044e970a8df50cb967a0ec68448cc28fbc
unknown <hjy4649@naver.com>
2023-05-04 11:22:59 +0900

    모든페이지test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Login from "./login";
 import Register from "./register";
+import Link from "next/link";
 import "./login.css";
 export default function main() {
   return (
@@ -8,6 +9,7 @@ export default function main() {
         <Login></Login>
         <Register></Register>
       </div>
+      <Link href="/main">일단 메인</Link>
     </div>
   );
 }
